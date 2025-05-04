d874bd29bebd83e10c90cef7cd26adf7eb347f97
ParsaKhaledi <pa.aerospace@gmail.com>
2025-05-04 19:04:49 +0330

    update files for left and right i_reverse_stereo_socket_order not worked

--- launchFiles/start_rtabmap_stereo.sh
+++ launchFiles/start_rtabmap_stereo.sh
@@ -12,15 +12,20 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 #      approx_sync:=true  wait_imu_to_init:=true  approx_sync_max_interval:=0.001  \
 #      qos:=2  rtabmapviz:=true  rviz:=false
 
-ros2 launch rtabmap_launch rtabmap.launch.py \
-     args:="-d --Optimizer/GravitySigma 0.1 --Vis/FeatureType 10  --Kp/DetectorStrategy 10  \
-     --Grid/MapFrameProjection true  --NormalsSegmentation false --Grid/MaxGroundHeight 1.0 \
-     --Grid/MaxObstacleHeight 2.0 --RGBD/StartAtOrigin true" \
-     stereo:=true  \
-    left_image_topic:=/left/image_rect    left_camera_info_topic:=/left/camera_info    \
-    right_image_topic:=/right/image_rect  right_camera_info_topic:=/right/camera_info   \
-    imu_topic:=/imu/data  frame_id:=oak-d-base-frame  \
-    approx_sync:=true  wait_imu_to_init:=true    \
-    qos:=2  rtabmapviz:=true  rviz:=false
+# ros2 launch rtabmap_launch rtabmap.launch.py \
+#      args:="-d --Optimizer/GravitySigma 0.1 --Vis/FeatureType 10  --Kp/DetectorStrategy 10  \
+#      --Grid/MapFrameProjection true  --NormalsSegmentation false --Grid/MaxGroundHeight 1.0 \
+#      --Grid/MaxObstacleHeight 2.0 --RGBD/StartAtOrigin true" \
+#      stereo:=true  \
+#     left_image_topic:=/left/image_rect    left_camera_info_topic:=/left/camera_info    \
+#     right_image_topic:=/right/image_rect  right_camera_info_topic:=/right/camera_info   \
+#     imu_topic:=/imu/data  frame_id:=oak-d-base-frame  \
+#     approx_sync:=true  wait_imu_to_init:=true    \
+#     qos:=2  rtabmapviz:=true  rviz:=false     # approx_sync_max_interval:=0.001 
 
-    # approx_sync_max_interval:=0.001 
+ros2 launch rtabmap_launch rtabmap.launch.py   \
+     args:='-d  --Optimizer/GravitySigma 0.1 --Vis/FeatureType 10 --Kp/DetectorStrategy 10 --Grid/MapFrameProjection true  \
+     --NormalsSegmentation false --Grid/MaxGroundHeight 0.5  --Grid/MaxObstacleHeight 2.2 --RGBD/StartAtOrigin true   '    \
+     rgb_topic:=/oak/rgb/image_raw   depth_topic:=/camera/depth/image_raw    camera_info_topic:=/camera/rgb/camera_info \
+     imu_topic:=/imu approx_sync:=true    \
+     use_sim_time:=true  qos:=2    rtabmapviz:=true     rviz:=false   subscribe_rgbd:=false
