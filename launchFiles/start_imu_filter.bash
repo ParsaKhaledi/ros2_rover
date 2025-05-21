ac23d3fddc25a1fe8f23549c0de2e94c5fc73355
ParsaKhaledi <pa.aerospace@gmail.com>
2025-05-21 15:21:41 +0330

    add source

--- launchFiles/start_imu_filter.bash
+++ launchFiles/start_imu_filter.bash
@@ -2,4 +2,5 @@
 sudo sed -i 's/use_mag: true/use_mag: false/g' $(ros2 pkg prefix imu_filter_madgwick)/share/imu_filter_madgwick/config/imu_filter.yaml && \
 sudo sed -i 's/publish_tf: true/publish_tf: false/g' $(ros2 pkg prefix imu_filter_madgwick)/share/imu_filter_madgwick/config/imu_filter.yaml && \
 sudo sed -i 's/world_frame: "enu"/world_frame: "map"/g' $(ros2 pkg prefix imu_filter_madgwick)/share/imu_filter_madgwick/config/imu_filter.yaml 
+source /home/rover/ws_rover/install/setup.bash && \
 ros2 launch imu_filter_madgwick imu_filter_component.launch.py
